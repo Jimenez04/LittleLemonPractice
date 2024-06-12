6f0506da9a501a0765337ac35f92d5aa3aa5b205
Jose Enrique Jimenez Soto <jimenez.fx@outlook.com>
2024-06-12 15:26:23 +0100

    REcover

--- src/assets/components/header.jsx
+++ src/assets/components/header.jsx
@@ -1,21 +1,21 @@
-// import React from "react";
-// import Logo from "../img/logo/Logo.png";
-// import Menu from './nav';
-// import "../css/header.css";
-// import { useNavigate } from "react-router-dom";
+import React from "react";
+import Logo from "../img/logo/Logo.png";
+import Menu from './nav';
+import "../css/header.css";
+import { useNavigate } from "react-router-dom";
 
-// export default function Header() {
-//     const navigate = useNavigate();
-//   return (
-//     <header className="flex flex_center_x">
-//       <div id="header_container" className="flex flex_center_y max_width flex_space_between">
-//         <div>
-//           <a href="" onClick={() => navigate("/")}>
-//             <img src={Logo} />
-//           </a>
-//         </div>
-//         <Menu />
-//       </div>
-//     </header>
-//   );
-// }
+export default function Header() {
+    const navigate = useNavigate();
+  return (
+    <header className="flex flex_center_x">
+      <div id="header_container" className="flex flex_center_y max_width flex_space_between">
+        <div>
+          <a href="" onClick={() => navigate("/")}>
+            <img src={Logo} />
+          </a>
+        </div>
+        <Menu />
+      </div>
+    </header>
+  );
+}
